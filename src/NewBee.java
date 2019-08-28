0c95fe203e48c61eeb67f47de2f5773182c976ab
hp <1286786871@qq.com>
2019-08-28 17:21:59 +0800

    加入 帅气

--- src/NewBee.java
+++ src/NewBee.java
@@ -1,8 +1,10 @@
 public class NewBee {
 
     private String s = "宗原牛逼！！！！";
+    private String d = "华华太帅了！！！";
 
     public void show(){
         System.out.println(this.s);
+        System.out.println(this.d);
     }
 }
