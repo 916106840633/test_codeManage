dce41e6911cfe257ca944f5633540bce3ebc785c
hp <1286786871@qq.com>
2019-08-28 13:03:09 +0800

    加入 test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World test !");
+        System.out.println("Hello World test!");
         test t = new test();
         t.change();
     }
